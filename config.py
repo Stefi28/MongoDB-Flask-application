c62d12012b8f2916cebf9687f473c04fd149512c
Stefani Kuzmanovska <stefanikuzmanovska@hotmail.com>
2023-02-08 19:07:13 +0100

    updated README.md

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ def connect_to_sqldb():
     conn = sqlite3.connect('semos_companies_data.db')
     return conn
 
-
 def connect_to_mongodb():
     client = MongoClient("mongodb://localhost:27017/")
     db = client["semos_companies_data"]
